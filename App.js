cc924359450b05d9481a4e531061f739c2636ce7
adave9 <ankurdave9@gmail.com>
2021-11-06 17:32:07 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Garage from './Garage';
+
+function App (){
+
+    return <div>
+                <p>This is my Garage !</p>
+                <br/>
+                <Garage/>
+                <Garage/>
+            </div>
+  
+}
+
+export default App;
---
+++ Garage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Garage(){
+return <div>
+              <p>I am a red car !</p>
+          </div>;
+
+}
+
+export default Garage;
